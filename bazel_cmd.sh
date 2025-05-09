a3aa6b43947e73c331fd590c0f728ddeaa5fa04a
Aleksey Korenkov <akorenkov@apple.com>
2025-05-09 16:07:11 -0700

    move "hiredis" into ray repo and build it as part of raylet build

--- bazel_cmd.sh
+++ bazel_cmd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-/opt/homebrew/opt/bazel@8/bin/bazel build --keep_going --verbose_failures --toolchain_resolution_debug=@bazel_tools//tools/cpp:toolchain_type -s --sandbox_debug --ios_sdk_version=19.0internal --apple_platform_type=ios --ios_multi_cpus=arm64e --platforms=//:ios --config=ios --ios_minimum_os=19.0 $1
+/opt/homebrew/opt/bazel@8/bin/bazel build --verbose_failures --toolchain_resolution_debug=@bazel_tools//tools/cpp:toolchain_type -s --sandbox_debug --ios_sdk_version=19.0internal --apple_platform_type=ios --ios_multi_cpus=arm64e --platforms=//:ios --config=ios --ios_minimum_os=19.0 $1
