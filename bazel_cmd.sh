f9145791ead2bf4b6af86de8966af28d46e85a9b
Abin Shahab <abin_shahab@apple.com>
2025-05-12 14:16:52 -0700

    temp commented out gcs_server

--- bazel_cmd.sh
+++ bazel_cmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 # rm -rf  /private/var/tmp/_bazel_abinshahab
-# rm -rf ~/.cache/bazel
-# bazel clean --expunge
+#rm -rf ~/.cache/bazel
+bazel clean --expunge
 bazel build --incompatible_disallow_empty_glob=false --noincompatible_disallow_ctx_resolve_tools --verbose_failures --toolchain_resolution_debug=@bazel_tools//tools/cpp:toolchain_type -s --sandbox_debug --ios_sdk_version=19.0internal --apple_platform_type=ios --ios_multi_cpus=arm64 --platforms=//:ios --config=ios --ios_minimum_os=19.0 $1
